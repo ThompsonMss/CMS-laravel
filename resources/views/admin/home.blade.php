ac58e7ec020b038a53d90f33c8422bc87b5d1c80
Thompson S <thompson.msds@gmail.com>
2021-05-04 22:04:32 -0300

    refactor: config adminlet

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,11 @@
-Pagina admin
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>Painel de Controller</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
