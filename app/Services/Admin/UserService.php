631eb26dd61b22a50208492a45674389ee22d123
Thompson S <thompson.msds@gmail.com>
2021-05-06 22:00:02 -0300

    add: grid users

--- app/Services/Admin/UserService.php
+++ app/Services/Admin/UserService.php
@@ -2,11 +2,26 @@
 
 namespace App\Services\Admin;
 
+use App\Models\User;
+
 class UserService
 {
+
+    private $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
     public function index()
     {
-        return view('admin.users.index');
+
+        $users = $this->model->all();
+
+        return view('admin.users.index', [
+            'users' => $users
+        ]);
     }
 
     public function create()
