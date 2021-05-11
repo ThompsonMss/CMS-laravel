47909d30bf76feb339348942ecc0bfcd6796f8f8
Thompson S <thompson.msds@gmail.com>
2021-05-11 10:21:21 -0300

    refactor: rename table settings

--- database/migrations/2021_05_11_131134_create_table_settings.php
+++ database/migrations/2021_05_11_131134_create_table_settings.php
@@ -13,7 +13,7 @@ class CreateTableSettings extends Migration
      */
     public function up()
     {
-        Schema::create('table_settings', function (Blueprint $table) {
+        Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100)->nullable(false);
             $table->text('content');
@@ -28,6 +28,6 @@ class CreateTableSettings extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_settings');
+        Schema::dropIfExists('settings');
     }
 }
